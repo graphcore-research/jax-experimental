54a5f120a6784be22d4ca6af3c1d11fad632fc7c
Sam Maddrell-Mander <samuelm@graphcore.ai>
2023-03-06 14:29:13 +0000

    First commit

--- examples/picoGPT/gpt2_ipu.py
+++ examples/picoGPT/gpt2_ipu.py
@@ -11,8 +11,6 @@ from utils import load_encoder_hparams_and_params
 
 config.FLAGS.jax_platform_name = 'ipu'
 
-
-
 def gelu(x):
     return 0.5 * x * (1 + np.tanh(np.sqrt(2 / np.pi) * (x + 0.044715 * x**3)))
 
