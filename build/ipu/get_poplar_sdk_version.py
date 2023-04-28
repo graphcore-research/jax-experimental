48152d2b416b2a7f9e47e9d0f853cf342a70fde7
Paul Balanca <paulb@graphcore.ai>
2023-04-28 08:51:43 +0000

    JAX IPU CI testing multiple Poplar SDKs.
    Testing 2 versions of Poplar SDKs is allowing smooth transitions.

---
+++ build/ipu/get_poplar_sdk_version.py
@@ -0,0 +1,21 @@
+"""Get Poplar SDK version outputted."""
+import re
+import subprocess
+
+
+def ipu_get_sdk_version() -> str:
+    """Get SDK version from popc command line."""
+    result = subprocess.run(['popc', '--version'], stdout=subprocess.PIPE)
+    popc_output = result.stdout.decode("utf-8")
+    sdk_version = re.search(r'POPLAR version\s*([\d.]+)', popc_output).group(1).strip()
+    return sdk_version
+
+
+def main():
+    sdk_version = ipu_get_sdk_version()
+    # Print short SDK version, stripped of .
+    print(sdk_version.replace(".", "").strip())
+
+
+if __name__ == "__main__":
+    main()
